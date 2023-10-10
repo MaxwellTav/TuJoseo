fd10f310bc9f6e65d672f986f4a12722bb07a285
Maxwell Tavares <maxwelltrt0709@gmail.com>
2023-10-09 23:23:26 -0400

    Agregar archivos de proyecto.

---
+++ TuJoseo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TuJoseo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

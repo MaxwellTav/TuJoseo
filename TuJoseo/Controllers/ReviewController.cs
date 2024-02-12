bd642c8c0fcf5706d527c0d13f8752f2917368b0
Maxwell Tavares <maxwelltrt0709@gmail.com>
2024-02-12 01:42:09 -0400

    Enviar datos a la vista y eliminar la notificación.

--- TuJoseo/Controllers/ReviewController.cs
+++ TuJoseo/Controllers/ReviewController.cs
@@ -1,12 +1,102 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using TuJoseo.Models;
 
 namespace TuJoseo.Controllers
 {
     public class ReviewController : Controller
     {
-        public IActionResult SetReview()
+        private readonly IConfiguration _configuration;
+        private readonly string ConnectionString;
+
+        public ReviewController(IConfiguration configuration)
         {
-            return View();
+            _configuration = configuration;
+            ConnectionString = _configuration.GetConnectionString("DB");
+        }
+
+        public IActionResult SetReview(int notificationID)
+        {
+            int userID = Convert.ToInt32(TempData["UserID"]);
+            UserModelReview UserReview = new UserModelReview();
+
+            //ID
+            string queryGetReview = $"Select * From PendingReviewTable Where PReviewID = {notificationID};";
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(queryGetReview, con))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                if (Convert.ToInt32(userID) == Convert.ToInt32(reader.GetInt32(1)))
+                                {
+                                    UserReview.UserID = Convert.ToInt32(reader.GetInt32(1));
+                                    UserReview.UserIDOther = Convert.ToInt32(reader.GetInt32(3));
+                                }
+                                else
+                                {
+                                    UserReview.UserID = Convert.ToInt32(reader.GetInt32(3));
+                                    UserReview.UserIDOther = Convert.ToInt32(reader.GetInt32(1));
+                                }
+
+                                UserReview.ProyectName = reader.GetString(5);
+                            }
+                        }
+                    }
+                }
+            }
+
+            //Nombre
+            string queryGetNameUsersMe = @$"SELECT u1.UserName AS Yo, u2.UserName AS Other 
+                                        FROM UserTable u1
+                                        INNER JOIN UserTable ur ON u1.UserID = {UserReview.UserID}
+                                        INNER JOIN UserTable u2 ON u2.UserID = {UserReview.UserIDOther}
+                                        WHERE ur.UserID = {UserReview.UserID};";
+
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(queryGetNameUsersMe, con))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                UserReview.UserName = reader.GetString(0);
+                                UserReview.UserNameOther = reader.GetString(1);
+                            }
+                        }
+                    }
+                }
+            }
+
+            TempData["UserID"] = userID;
+            return View(UserReview);
+        }
+
+        public IActionResult DeleteNotification(int notificationID)
+        {
+            string userID = TempData["UserID"].ToString();
+            string queryDeleteNotification = $"Delete From PendingReviewTable Where PReviewID = {notificationID};";
+
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(queryDeleteNotification, con))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            TempData["UserID"] = userID;
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ TuJoseo/Models/UserModelReview.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Principal;
+
+namespace TuJoseo.Models
+{
+    public class UserModelReview : UserModel
+    {
+        public int UserIDOther { get; set; }
+        public string UserNameOther { get; set; }
+        public string ProyectName { get; set; }
+    }
+}
